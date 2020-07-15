089286f68ddb3db231a0291992bf75379860e1de
hyperreality <lmt@fastmail.com>
2020-07-15 16:24:41 +0100

    Add smaller dictionary

--- solver_test.go
+++ solver_test.go
@@ -20,10 +20,8 @@ func TestDoublePatternSearch(t *testing.T) {
     var words = importWords("dictionaries/merged.txt")
 
     var pattern1 = ".osmos"
-    var pos1 = 3
     var pattern2 = "m....o..sm"
-    var pos2 = 9
-    var results = doublePatternSearch(words, pattern1, pattern2, pos1, pos2)
+    var results = doublePatternSearch(words, pattern1, pattern2)
     var expected = "cosmos:metabolism\n"
 
     if results != expected {
